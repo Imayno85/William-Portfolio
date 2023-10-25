c4d0e499b5027a0025f3ba36be7e7af9291e0681
Onyami George William <Imayno85@gmail.com>
2023-10-25 12:34:40 +0300

    portfolio_commit3_complete

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -14,10 +14,9 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
+    <Float speed={0.1} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={1} />
       <directionalLight position={[0, 0, 0.05]} />
-
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
@@ -39,14 +38,20 @@ const Ball = (props) => {
 };
 
 const BallCanvas = ({ icon }) => {
-  <Canvas frameloop="demand" dpr={[1, 2]} gl={{ preserveDrawingBuffer: true }}>
-    <Suspense fallback={<CanvasLoader />}>
-      <OrbitControls enableZoom={false} />
-      <Ball imgUrl={icon} />
-    </Suspense>
+  return (
+    <Canvas
+      frameloop="demand"
+      dpr={[1, 2]}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
 
-    <Preload all />
-  </Canvas>;
+      <Preload all />
+    </Canvas>
+  );
 };
 
 export default BallCanvas;
